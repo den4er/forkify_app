78fb57ec060a8f1619e61f2a3b2a6599e9f16cae
den4er <den4er@mail.ru>
2021-12-11 19:15:52 +0300

    add git config

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import icons from 'url:../img/icons.svg';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
